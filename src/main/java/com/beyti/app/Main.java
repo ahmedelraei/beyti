dd9b678c07b531071acb7676707bc3c805a12a33
ahmed <ahmedelrai18@gmail.com>
2022-12-23 22:20:33 +0200

    Database Diagrams

--- src/main/java/com/beyti/app/Main.java
+++ src/main/java/com/beyti/app/Main.java
@@ -4,6 +4,26 @@ import com.beyti.utils.DB;
 import javax.swing.*;
 import java.sql.Connection;
 
+// @author: Ahmed Hatem Mohamed Elraei
+// Class: 17
+// ID: 12200207
+
+// @author: Ahmed Abduljalil
+// Class: 17
+// ID: 12200550
+
+// @author: Peter Halim
+// Class: 17
+// ID:
+
+// @author: Ahmed Bahgat
+// Class: 17
+// ID:
+
+// @author: Taha Mohsen
+// Class: 17
+// ID:
+
 public class Main {
     public static void main(String[] args) {
         Connection conn = DB.connect();
