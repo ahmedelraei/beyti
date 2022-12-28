832a5bb0059c2710240308efcf9a792dfe802fc1
ahmed <ahmedelrai18@gmail.com>
2022-12-28 22:22:06 +0200

    .

--- src/main/java/com/beyti/app/Main.java
+++ src/main/java/com/beyti/app/Main.java
@@ -4,25 +4,6 @@ import com.beyti.utils.DB;
 import javax.swing.*;
 import java.sql.Connection;
 
-// @author: Ahmed Hatem Mohamed Elraei
-// Class: 17
-// ID: 12200207
-
-// @author: Ahmed Abduljalil
-// Class: 17
-// ID: 12200550
-
-// @author: Peter Halim
-// Class: 17
-// ID: 12200242
-
-// @author: Ahmed Bahgat
-// Class: 17
-// ID: 12200187
-
-// @author: Taha Mohsen
-// Class: 17
-// ID:
 
 public class Main {
     public static void main(String[] args) {
